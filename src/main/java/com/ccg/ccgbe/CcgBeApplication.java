8f2b8ccf02e3458d2e9725c232accbeeb13ffb41
Stefan Wetzig <stefanwetzig@yahoo.de>
2022-12-19 13:07:17 +0100

    first commit

---
+++ src/main/java/com/ccg/ccgbe/CcgBeApplication.java
@@ -0,0 +1,13 @@
+package com.ccg.ccgbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcgBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcgBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ccg/ccgbe/CcgBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccg.ccgbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcgBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
